4163bafb48b13e85aa12b844143028a753513aac
Puspalal <puspalalnewar79@gmail.com>
2025-02-06 12:12:46 +0530

    Created mobile friendly

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ let voices = [];
 
 let voiceSelect = document.querySelector("select");
 
-window.speechSynthesis.onvoiceschanged = ()=>{
+window.speechSynthesis.onvoiceschanged = () => {
     voices = window.speechSynthesis.getVoices();
     speech.voice = voices[0];
 
-    voices.forEach((voice, i)=> (voiceSelect.options[i] = new Option(voice.name, i)));
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
 };
 
-voiceSelect.addEventListener("change", ()=>{
+voiceSelect.addEventListener("change", () => {
     speech.voice = voices[voiceSelect.value];
 })
 
-document.querySelector("button").addEventListener("click", ()=>{
+document.querySelector("button").addEventListener("click", () => {
     speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
 })
